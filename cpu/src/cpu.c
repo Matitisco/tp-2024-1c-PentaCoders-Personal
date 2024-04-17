a3e37a3400a569987eadf1905f3e00fa170aa31c
Ignacio Joaquin Riolffi <iriolffi@frba.utn.edu.ar>
2024-04-17 18:06:11 +0000

    Problema con segmentation de cpu arreglado

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -1,62 +1,62 @@
 #include <../include/cpu.h>
 
-int main(int argc, char* argv[]) {
-    char* PUERTO_MEMORIA, IP;
+int main(int argc, char *argv[])
+{
+	char *PUERTO_MEMORIA, *IP;
 	int CONEXION_MEMORIA;
-    t_config* CONFIG;
-	t_log* logger;
-	
-	logger = log_create("Cpu.log", "CPU", 1, LOG_LEVEL_DEBUG);
+	t_config *config;
+	t_log *logger;
 
+	logger = log_create("Cpu.log", "CPU", 1, LOG_LEVEL_DEBUG);
+	logger = iniciar_logger("cpu.log", "CPU");
 	// CONFIG
-	CONFIG = iniciar_config("../cpu.config");
-	IP = config_get_string_value(CONFIG,"IP_MEMORIA");
-	PUERTO_MEMORIA = config_get_string_value(CONFIG,"PUERTO_MEMORIA");
-	CONEXION_MEMORIA = crear_conexion(logger,"Memoria",IP, PUERTO_MEMORIA);
-    // CPU SE CONECTA A MEMORIA
+	config = iniciar_config("../cpu.config");
+	IP = config_get_string_value(config, "IP_MEMORIA");
+	PUERTO_MEMORIA = config_get_string_value(config, "PUERTO_MEMORIA");
+	CONEXION_MEMORIA = crear_conexion(logger, "Memoria", IP, PUERTO_MEMORIA);
+	// CPU SE CONECTA A MEMORIA
 	/*
 	Falta que CPU inicie servidor como INTERRUPT y como DISPATCH
 	Kernel a CPU está a la mitad
 	*/
 
-    /* MEMORIA COMO SERVER DE CPU*/
-	logger = iniciar_logger("cpu.log", "CPU");
-	enviar_mensaje("CPU Se conecto a Memoria",CONEXION_MEMORIA);
+	/* MEMORIA COMO SERVER DE CPU*/
 
-    // CPU COMO SERVER DE KERNEL
-   	int server_fd = iniciar_servidor(logger,"SERVIDOR CPU",IP, PUERTO_MEMORIA);
+	enviar_mensaje("CPU Se conecto a Memoria", CONEXION_MEMORIA);
+
+	// CPU COMO SERVER DE KERNEL
+	int server_fd = iniciar_servidor(logger, "SERVIDOR CPU", IP, PUERTO_MEMORIA);
 	log_info(logger, "Servidor CPU listo para recibir al cliente KERNEL");
-	int cliente_fd = esperar_cliente(logger, "Kernel" ,server_fd);
+	int cliente_fd = esperar_cliente(logger, "Kernel", server_fd);
 
-	t_list* lista;
-	while (1) {
+	t_list *lista;
+	while (1)
+	{
 		int cod_op = recibir_operacion(cliente_fd);
-		switch (cod_op) {
-			case MENSAJE:
-				recibir_mensaje(cliente_fd);
-				break;
-			case PAQUETE:
-				lista = recibir_paquete(cliente_fd);
-				log_info(logger, "Me llegaron los siguientes valores:\n");
-				list_iterate(lista, (void*) iterator);
-				break;
-			case -1:
-				log_error(logger, "el cliente se desconecto. Terminando servidor");
-				//return EXIT_FAILURE;
-			default:
-				log_warning(logger,"Operacion desconocida. No quieras meter la pata");
-				break;
+		switch (cod_op)
+		{
+		case MENSAJE:
+			recibir_mensaje(cliente_fd);
+			break;
+		case PAQUETE:
+			lista = recibir_paquete(cliente_fd);
+			log_info(logger, "Me llegaron los siguientes valores:\n");
+			list_iterate(lista, (void *)iterator);
+			break;
+		case -1:
+			log_error(logger, "el cliente se desconecto. Terminando servidor");
+			return EXIT_FAILURE;
+		default:
+			log_warning(logger, "Operacion desconocida. No quieras meter la pata");
+			break;
 		}
 	}
 	return EXIT_SUCCESS;
-    
 }
 
-
-void iterator(char* value){
-	log_info(logger,"%s", value);
+void iterator(char *value)
+{
+	log_info(logger, "%s", value);
 }
-
-
 
 /*CPU es cliente de memoria y servidor de Kernel*/
