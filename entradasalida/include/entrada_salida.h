a0c7c07efa3753360177a5690677e637ec0e835a
Ignacio Joaquin Riolffi <iriolffi@frba.utn.edu.ar>
2024-05-05 22:39:11 +0000

    Hago implementacion de crear interfaces, falta que reciban de kernel las instrucciones

--- entradasalida/include/entrada_salida.h
+++ entradasalida/include/entrada_salida.h
@@ -8,36 +8,71 @@
 #include "../../utils/include/conexiones.h"
 
 extern t_log *logger;
-int conexion_kernel,conexion_memoria_desde_IO;
+int conexion_kernel, conexion_memoria_desde_IO;
 int socket_memoria;
 int socket_kernel;
+/*
 
-struct config_io{
-	t_config* config;
-	char *ip;
-	char *puerto_kernel;
-	char *puerto_memoria;
-	char* tipo_interfaz;
-	//int tiempo_unidad_trabajo;
+	PRUEBA CON DIALFS QUE ANDA TODO BIEN Y SE LEEN LOS ARCHIVOS
+	printf("%d\n", interfaz_io->tipo_interfaz);
+	printf("%d\n", interfaz_io->tiempo_unidad_trabajo);
+	// printf("%d\n", interfaz_io->retraso_compactacion);
+	// printf("%d\n", interfaz_io->puerto_memoria);
+	printf("%d\n", interfaz_io->puerto_kernel);
+	// printf("%s\n", interfaz_io->path_base_dialfs);
+	// printf("%s\n", interfaz_io->ip_memoria);
+	printf("%s\n", interfaz_io->ip_kernel);
+	// printf("%d\n", interfaz_io->block_size);
+	// printf("%d\n", interfaz_io->block_count);
+
+*/
+typedef struct
+{
+	t_config *config;
+	// char *ip;
+	int puerto_kernel;
+	int puerto_memoria;
+	char *tipo_interfaz;
 	char *ip_kernel;
 	char *ip_memoria;
-	//path_base_dialfs;
-	//int block_size;
-	//int block_count;
-};
+	char *path_base_dialfs;
+	int tiempo_unidad_trabajo;
+	int block_size;
+	int block_count;
+	int retraso_compactacion;
+} config_io;
 
-struct config_io* config_io(){
-	struct config_io* valores_config = malloc(sizeof(struct config_io));
+typedef enum
+{
+	GENERICA,
+	STDIN,
+	STDOUT,
+	DIALFS,
+} enum_interfaz;
 
-	//creo el config
-	valores_config->config = iniciar_config("entrada_salida.config");
+typedef struct
+{
+	enum_interfaz tipo_interfaz;
+	int tiempo_unidad_trabajo;
+	char *ip_kernel;
+	int puerto_kernel;
+	char *ip_memoria;
+	int puerto_memoria;
+	char *path_base_dialfs;
+	int block_size;
+	int block_count;
+	int retraso_compactacion;
+} t_interfaz;
 
-	valores_config->ip = config_get_string_value(valores_config->config, "IP_KERNEL");
-	valores_config->puerto_kernel = config_get_string_value(valores_config->config, "PUERTO_KERNEL");
-	valores_config->puerto_memoria = config_get_string_value(valores_config->config, "PUERTO_MEMORIA");
-
-    
-	return valores_config;
-}
-
+config_io *inicializar_config_IO(char *PATH);
+void levantar_interfaz(char *nombre, char *PATH);
+t_interfaz *crear_interfaz_generica(config_io *config);
+t_interfaz *crear_interfaz_stdin(config_io *config);
+t_interfaz *crear_interfaz_stdout(config_io *config);
+t_interfaz *crear_interfaz_dialfs(config_io *config);
+enum_interfaz asignarInterfaz(char *nombre_Interfaz);
+void arrancar_interfaz_generica(t_interfaz *interfaz_io);
+void arrancar_interfaz_stdin(t_interfaz *interfaz_io);
+void arrancar_interfaz_stdout(t_interfaz *interfaz_io);
+void arrancar_interfaz_dialfs(t_interfaz *interfaz_io);
 #endif
--- entradasalida/src/entrada_salida.c
+++ entradasalida/src/entrada_salida.c
@@ -1,29 +1,180 @@
 #include "../include/entrada_salida.h"
 
+t_interfaz *interfaz_io;
+
 int main(int argc, char *argv[])
 {
-	struct config_io *valores_config = config_io();
+
 	logger = iniciar_logger("entrada_salida.log", "ENTRADA_SALIDA");
+	// config_io *valores_config = inicializar_config_IO("entrada_salida.config");
+	// conexion_memoria_desde_IO = levantarCliente(logger, "Servidor Memoria", valores_config->ip_memoria, valores_config->puerto_memoria, "ENTRADA/SALIDA se conecta a Memoria");
+	// conexion_kernel = levantarCliente(logger, "Servidor Kernel", valores_config->ip_kernel, valores_config->puerto_kernel, "ENTRADA/SALIDA se conecta a Kernel");
+	// esto previo no va, ya que cada interfaz se va a prender una vez que se levanta en el sistema
 
-	conexion_memoria_desde_IO = levantarCliente(logger, "Servidor Memoria", valores_config->ip_memoria, valores_config->puerto_memoria, "ENTRADA/SALIDA se conecta a Memoria");
-	conexion_kernel = levantarCliente(logger, "Servidor Kernel", valores_config->ip_kernel, valores_config->puerto_kernel, "ENTRADA/SALIDA se conecta a Kernel");
-	terminar_programa(conexion_memoria_desde_IO, logger, valores_config->config);
-	liberarConexion(conexion_kernel);
+	// ACA ARRANCA EL PROGRAMA
 
+	char *nombre_interfaz = readline("Ingrese el nombre de la interfaz: ");
+	char *path_configuracion = readline("Ingrese PATH con la configuracion de la interfaz: ");
+	//  LEVANTAR INTERFAZ
+	levantar_interfaz(nombre_interfaz, path_configuracion); // recibo el nombre id de la interfaz y su archivo de configuracion
+
+	// esto no iria
+	// terminar_programa(conexion_memoria_desde_IO, logger, valores_config->config);
+	// liberarConexion(conexion_kernel);
+	/* creo de abse 4 instancias de los tipos de interfaces que existen, y luego dependiendo el tipo que se vaya a crear, luego borro los otros*/
 }
 
 // FUNCIONES
-
-void leer_consola(t_log *logger)
+void levantar_interfaz(char *nombre, char *PATH)
 {
-	char *leido;
-	leido = readline("> ");
-	log_info(logger, "%s", leido);
-	while (strcmp(leido, "") != 0)
+	// primero leo el path y obtengo el tipo de interfaz luego creo segun el tipo de interfaz
+	// aca solo inicializo el archivo de config
+	config_io *config_interfaz = inicializar_config_IO(PATH);
+	switch (asignarInterfaz(config_interfaz->tipo_interfaz))
 	{
-		free(leido);
-		leido = readline("> ");
-		log_info(logger, "%s", leido);
+	// aca creo segun el tipo de interfaz con su config
+	case GENERICA:
+		interfaz_io = crear_interfaz_generica(config_interfaz);
+		arrancar_interfaz_generica(interfaz_io);
+		break;
+	case STDIN:
+		interfaz_io = crear_interfaz_stdin(config_interfaz);
+		arrancar_interfaz_stdin(interfaz_io);
+		break;
+	case STDOUT:
+		interfaz_io = crear_interfaz_stdout(config_interfaz);
+		arrancar_interfaz_stdout(interfaz_io);
+		break;
+	case DIALFS:
+		interfaz_io = crear_interfaz_dialfs(config_interfaz);
+		arrancar_interfaz_dialfs(interfaz_io);
+		break;
 	}
-	free(leido);
 }
+
+t_interfaz *crear_interfaz_generica(config_io *config)
+{
+	t_interfaz *interfaz;
+	interfaz = malloc(sizeof(t_interfaz));
+	interfaz->tipo_interfaz = asignarInterfaz(config->tipo_interfaz);
+	interfaz->tiempo_unidad_trabajo = config->tiempo_unidad_trabajo;
+	interfaz->ip_kernel = config->ip_kernel;
+	interfaz->puerto_kernel = config->puerto_kernel;
+	return interfaz;
+}
+
+t_interfaz *crear_interfaz_stdin(config_io *config)
+{
+	t_interfaz *interfaz = malloc(sizeof(t_interfaz));
+	interfaz->tipo_interfaz = asignarInterfaz(config->tipo_interfaz);
+	interfaz->ip_kernel = config->ip_kernel;
+	interfaz->puerto_kernel = config->puerto_kernel;
+	interfaz->ip_memoria = config->ip_memoria;
+	interfaz->puerto_memoria = config->puerto_memoria;
+	return interfaz;
+}
+
+t_interfaz *crear_interfaz_stdout(config_io *config)
+{
+	t_interfaz *interfaz;
+	interfaz = malloc(sizeof(t_interfaz));
+	interfaz->tipo_interfaz = asignarInterfaz(config->tipo_interfaz);
+	interfaz->tiempo_unidad_trabajo = config->tiempo_unidad_trabajo;
+	interfaz->ip_kernel = config->ip_kernel;
+	interfaz->puerto_kernel = config->puerto_kernel;
+	interfaz->ip_memoria = config->ip_memoria;
+	interfaz->puerto_memoria = config->puerto_memoria;
+	return interfaz;
+}
+
+t_interfaz *crear_interfaz_dialfs(config_io *config)
+{
+	t_interfaz *interfaz;
+	interfaz = malloc(sizeof(t_interfaz));
+	interfaz->tipo_interfaz = asignarInterfaz(config->tipo_interfaz);
+	interfaz->tiempo_unidad_trabajo = config->tiempo_unidad_trabajo;
+	interfaz->ip_kernel = config->ip_kernel;
+	interfaz->puerto_kernel = config->puerto_kernel;
+	interfaz->ip_memoria = config->ip_memoria;
+	interfaz->puerto_memoria = config->puerto_memoria;
+	interfaz->path_base_dialfs = config->path_base_dialfs;
+	interfaz->block_size = config->block_size;
+	interfaz->block_count = config->block_count;
+	interfaz->retraso_compactacion = config->retraso_compactacion;
+	return interfaz;
+}
+
+enum_interfaz asignarInterfaz(char *nombre_Interfaz)
+{
+	if (strcmp(nombre_Interfaz, "GENERICA") == 0)
+	{
+		return GENERICA;
+	}
+	if (strcmp(nombre_Interfaz, "STDIN") == 0)
+	{
+		return STDIN;
+	}
+	if (strcmp(nombre_Interfaz, "STDOUT") == 0)
+	{
+		return STDOUT;
+	}
+	if (strcmp(nombre_Interfaz, "DIALFS") == 0)
+	{
+		return DIALFS;
+	}
+	return 0; // no encontro la interfaz
+}
+
+config_io *inicializar_config_IO(char *PATH)
+{
+	config_io *valores_config = malloc(sizeof(config_io));
+
+	// creo el config
+	valores_config->config = iniciar_config(PATH);
+	// configuraciones basicas
+	valores_config->tipo_interfaz = config_get_string_value(valores_config->config, "TIPO_INTERFAZ");
+	valores_config->ip_kernel = config_get_string_value(valores_config->config, "IP_KERNEL");
+	valores_config->puerto_kernel = atoi(config_get_string_value(valores_config->config, "PUERTO_KERNEL"));
+	if (strcmp(valores_config->tipo_interfaz, "GENERICA") == 0)
+	{
+		valores_config->tiempo_unidad_trabajo = config_get_int_value(valores_config->config, "TIEMPO_UNIDAD_TRABAJO");
+	}
+	if (strcmp(valores_config->tipo_interfaz, "STDIN") == 0)
+	{
+		valores_config->ip_memoria = config_get_string_value(valores_config->config, "IP_MEMORIA");
+		valores_config->puerto_memoria = atoi(config_get_string_value(valores_config->config, "PUERTO_MEMORIA"));
+	}
+	if (strcmp(valores_config->tipo_interfaz, "STDOUT") == 0)
+	{
+		valores_config->tiempo_unidad_trabajo = config_get_int_value(valores_config->config, "TIEMPO_UNIDAD_TRABAJO");
+		valores_config->ip_memoria = config_get_string_value(valores_config->config, "IP_MEMORIA");
+		valores_config->puerto_memoria = atoi(config_get_string_value(valores_config->config, "PUERTO_MEMORIA"));
+	}
+	if (strcmp(valores_config->tipo_interfaz, "DIALFS") == 0)
+	{
+		valores_config->tiempo_unidad_trabajo = config_get_int_value(valores_config->config, "TIEMPO_UNIDAD_TRABAJO");
+		valores_config->ip_memoria = config_get_string_value(valores_config->config, "IP_MEMORIA");
+		valores_config->puerto_memoria = atoi(config_get_string_value(valores_config->config, "PUERTO_MEMORIA"));
+		valores_config->path_base_dialfs = config_get_string_value(valores_config->config, "PATH_BASE_DIALFS");
+		valores_config->block_size = config_get_int_value(valores_config->config, "BLOCK_SIZE");
+		valores_config->block_count = config_get_int_value(valores_config->config, "BLOCK_COUNT");
+		valores_config->retraso_compactacion = config_get_int_value(valores_config->config, "RETRASO_COMPACTACION");
+	}
+	return valores_config;
+}
+
+void arrancar_interfaz_generica(t_interfaz *interfaz_io)
+{
+	int unidad_tiempo = interfaz_io->tiempo_unidad_trabajo;
+	char *ip = interfaz_io->ip_kernel;
+	int puerto = interfaz_io->puerto_kernel;
+	// 1-conectar con kernel
+	conexion_kernel = levantarCliente(logger, "KERNEL", ip, puerto, "Hola, soy una interfaz generica");
+	// 2-esperar que kernel envie un mensaje
+	// 3-atender el mensaje que envia kernel
+	// 4-responder al kernel que termine
+	// 5-vuelvo al paso 2
+}
+void arrancar_interfaz_stdin(t_interfaz *interfaz_io) {}
+void arrancar_interfaz_stdout(t_interfaz *interfaz_io) {}
+void arrancar_interfaz_dialfs(t_interfaz *interfaz_io) {}
