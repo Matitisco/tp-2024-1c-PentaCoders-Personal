e0ccc7f94be7be5f26f5aa0137f180f83b7c2b01
Ignacio Joaquin Riolffi <iriolffi@frba.utn.edu.ar>
2024-06-08 20:05:38 +0000

    Avances en TLB, faltan algoritmos

--- cpu/include/tlb.h
+++ cpu/include/tlb.h
@@ -3,10 +3,28 @@
 
 #include "cpu.h"
 
-typedef struct{
+// ESTRUCTURAS
+typedef struct
+{
     int pid;
     int pagina;
     int marco;
-}t_tlb;
+} tlb_entrada;
+
+typedef struct
+{
+    tlb_entrada *entradas;
+    char *algoritmo;
+    int cant_entradas;
+} tlb_tabla;
+
+// FUNCIONES
+
+void tlb_iniciar(char *algoritmo, int cant_entradas);
+void tlb_crear(char *algoritmo, int cant_entradas);
+int tlb_consultar_info_pagina(int pagina_buscada);
+void tlb_agregar_entrada(int pid, int pagina, int marco);
+
+
 
 #endif
--- cpu/src/tlb.c
+++ cpu/src/tlb.c
@@ -1,5 +1,107 @@
 #include "../include/tlb.h"
 
-void obtener_entradas_tlb(){
-    //int entradas_tlb = valores_config_cpu.cantidad_entradas_tlb;
+tlb_tabla *tlb;
+
+void tlb_iniciar(char *algoritmo, int cant_entradas)
+{
+    if (tlb_cant_entradas <= 0)
+    {
+        log_info(logger, "TLB - DESHABILITADA");
+    }
+    else
+    {
+        tlb_crear(algoritmo, cant_entradas);
+        log_info(logger, "TLB - ENTRADAS: <%d> ", tlb->cant_entradas);
+    }
+}
+
+void tlb_crear(char *algoritmo, int cant_entradas)
+{
+    tlb = malloc(sizeof(tlb_tabla));
+
+    tlb->algoritmo = *algoritmo;
+    tlb->entradas = malloc(sizeof(tlb_entrada) * cant_entradas);
+    tlb->cant_entradas = cant_entradas;
+
+    for (int i = 0; i < cant_entradas; i++)
+    {
+        tabla_tlb->entradas[i] = NULL;
+    }
+}
+
+int tlb_consultar_info_pagina(int pagina_buscada)
+{
+
+    for (int i = 0; i < tlb->cant_entradas; i++)
+    {
+        int pagina_tlb = tlb->entradas[i].pagina;
+
+        if (pagina_buscada = pagina_tlb)
+        {
+            return tlb->entradas[i].marco; // TLB HIT
+        }
+    }
+
+    return -1; // TLB MISS
+}
+
+void tlb_agregar_entrada(int pid, int pagina, int marco)
+{
+    int entrada_libre;
+    tlb_entrada *entrada;
+    if ((entrada_libre = entrada_obtener()) < 0) // no hay lugares libres
+    {
+        entrada = entrada_crear(pid, pagina, marco);
+        tlb_reemplazar(entrada);
+        free(entrada);
+    }
+    else // hay lugares disponibles
+    {
+        entrada = entrada_crear(pid, pagina, marco);
+        tlb->entradas[entrada_libre] = entrada;
+        free(entrada);
+    }
+}
+
+void tlb_reemplazar(tlb_entrada *entrada)
+{
+    if (tlb->algoritmo == "FIFO")
+    {
+        log_info(logger, "TLB - REEMPLAZO POR FIFO");
+        tlb_reemplazo_fifo(entrada);
+    }
+    else if (tlb->algoritmo == "LRU")
+    {
+        log_info(logger, "TLB - REEMPLAZO POR LRU");
+        tlb_reemplazo_lru(entrada);
+    }
+}
+
+void tlb_reemplazo_fifo(tlb_entrada *entrada) // IMPLEMENTAR
+{
+}
+
+void tlb_reemplazo_lru(tlb_entrada *entrada) // IMPLEMENTAR
+{
+}
+
+tlb_entrada *entrada_crear(int pid, int pagina, int marco)
+{
+    tlb_entrada *entrada_nueva = malloc(sizeof(tlb_entrada));
+    entrada_nueva->pid = pid;
+    entrada_nueva->pagina = pagina;
+    entrada_nueva->marco = marco;
+    return entrada_nueva;
+}
+
+int entrada_obtener()
+{
+    for (int i = 0; i < tlb->cant_entradas; i++)
+    {
+        if (tlb->entradas[i] == NULL)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
