a83e35ad986d395e432bb6e33a7d3eac1d9719fd
Matitisco <matiscornia@frba.utn.edu.ar>
2024-07-10 04:31:06 +0000

    Ejecutar Scripts empezo a funcionar bien

--- kernel/src/operaciones.c
+++ kernel/src/operaciones.c
@@ -6,14 +6,30 @@ uint32_t PID_GLOBAL = 0;
 
 void ejecutar_script(char *PATH)
 {
+
     char *linea_script = string_new();
     char **lineas_script = string_array_new();
     char linea[1024];
-    FILE *archivo_script = fopen(PATH, "r");
+
+
+    char *path_completo = string_new();
+    char *ruta_acceso[1024];
+
+    if (getcwd(ruta_acceso, sizeof(ruta_acceso)) == NULL)
+    {
+        log_info(logger, "No se pudo obtener la raiz");
+        return NULL;
+    }
+
+    string_append(&path_completo, ruta_acceso);    
+    string_append(&path_completo, "/../memoria/pruebas/checkpoint_3/scripts/");
+    string_append(&path_completo, PATH);
+
+    FILE *archivo_script = fopen(path_completo, "r");
 
     if (archivo_script == NULL)
     {
-        log_info(logger, "No se pudo leer el script con PATH: %s", PATH);
+        log_info(logger, "No se pudo leer el script con PATH: %s", path_completo);
         iniciar_consola_interactiva();
     }
     while (fgets(linea, sizeof(linea), archivo_script) != NULL)
@@ -46,6 +62,7 @@ void ejecutar_script(char *PATH)
             proceso_estado();
         }
     }
+    free(path_completo);
     free(lineas_script);
     free(linea_script);
     fclose(archivo_script);
