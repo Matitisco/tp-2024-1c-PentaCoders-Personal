f4caf314f29d7e04bc7021d2324735d2c87cdfcf
NahuelLazarte <clazarte@frba.utn.edu.ar>
2024-07-12 00:10:33 -0300

    correccion pequeña

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -186,7 +186,7 @@ void pedido_frame_mmu(int cliente_cpu)
 
     t_tabla_paginas *tabla = buscar_en_lista_global(pid);
 
-    if (list_size(tabla->paginas_proceso) <= pagina)
+    if (pagina >= list_size(tabla->paginas_proceso)  )
     {
         enviar_op_code(cliente_cpu, PEDIDO_FRAME_INCORRECTO);
         return;
